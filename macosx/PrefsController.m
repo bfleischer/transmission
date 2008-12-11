bfleischer/transmission

commit 52c3e6963872b2c52688fc8d9c89471d61165089

#1157 ability to pick a random port in libtransmission; mac preference to pick a random port on launch and on button click
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@7352 f4695dd4-2c0a-0410-b89c-da849a56a58e

macosx/PrefsController.m
@@ -111,6 +111,10 @@ tr_handle * fHandle;
             [fDefaults removeObjectForKey: @"DownloadChoice"];
         }
         
+        //save a new random port
+        if ([fDefaults boolForKey: @"RandomPort"])
+            [fDefaults setInteger: tr_sessionGetPeerPort(fHandle) forKey: @"BindPort"];
+        
         //set auto import
         NSString * autoPath;
         if ([fDefaults boolForKey: @"AutoImport"] && (autoPath = [fDefaults stringForKey: @"AutoImportDirectory"]))
@@ -338,6 +342,14 @@ tr_handle * fHandle;
     
     fPeerPort = -1;
     [self updatePortStatus];
+}
+
+- (void) randomPort: (id) sender
+{
+    tr_port port = tr_sessionSetPeerPortRandom(fHandle);
+    
+    [fPortField setIntValue: port];
+    [self setPort: nil];
 }
 
 - (void) setNat: (id) sender
@@ -1036,7 +1048,7 @@ tr_handle * fHandle;
     [fDefaults setBool: pex forKey: @"PEXGlobal"];
     
     //port
-    int port = tr_sessionGetPeerPort(fHandle);
+    tr_port port = tr_sessionGetPeerPort(fHandle);
     [fDefaults setInteger: port forKey: @"BindPort"];
     
     BOOL nat = tr_sessionIsPortForwardingEnabled(fHandle);

libtransmission/session.c
@@ -39,6 +39,9 @@
 #include "web.h"
 #include "crypto.h"
 
+#define PORT_RANDOM_MIN 1024
+#define PORT_RANDOM_MAX 65535
+
 /* Generate a peer id : "-TRxyzb-" + 12 random alphanumeric
    characters, where x is the major version number, y is the
    minor version number, z is the maintenance number, and b
@@ -275,6 +278,11 @@ tr_sessionInitFull( const char *       configDir,
     /* Initialize rate and file descripts controls */
 
     tr_fdInit( globalPeerLimit );
+    
+    /* random port */
+    if ( publicPort == -1 )
+        publicPort = tr_cryptoWeakRandInt(PORT_RANDOM_MAX - PORT_RANDOM_MIN + 1) + PORT_RANDOM_MIN;
+    
     h->shared = tr_sharedInit( h, isPortForwardingEnabled, publicPort );
     h->isPortSet = publicPort >= 0;
 
@@ -421,6 +429,14 @@ tr_sessionSetPeerPort( tr_session * session,
     data->session = session;
     data->port = port;
     tr_runInEventThread( session, tr_setBindPortImpl, data );
+}
+
+tr_port
+tr_sessionSetPeerPortRandom( tr_session * session )
+{
+    const tr_port port = tr_cryptoWeakRandInt(PORT_RANDOM_MAX - PORT_RANDOM_MIN + 1) + PORT_RANDOM_MIN;
+    tr_sessionSetPeerPort( session, port);
+    return port;
 }
 
 tr_port

libtransmission/transmission.h
@@ -204,6 +204,7 @@ tr_encryption_mode;
  *
  * @param publicPort
  *  Port number to open for incoming peer connections.
+ *  -1 for random port.
  *  #TR_DEFAULT_PORT is the default.
  *
  * @param encryptionMode
@@ -538,6 +539,8 @@ tr_bool tr_sessionIsPortForwardingEnabled( const tr_session  * session );
 
 void  tr_sessionSetPeerPort( tr_session  * session,
                              tr_port       port);
+
+tr_port tr_sessionSetPeerPortRandom( tr_session  * session );
 
 tr_port tr_sessionGetPeerPort( const tr_session * session );
 

macosx/Controller.m
@@ -213,7 +213,7 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
                                 [[[fDefaults stringForKey: @"DownloadFolder"] stringByExpandingTildeInPath] UTF8String],
                                 [fDefaults boolForKey: @"PEXGlobal"],
                                 [fDefaults boolForKey: @"NatTraversal"],
-                                [fDefaults integerForKey: @"BindPort"],
+                                [fDefaults boolForKey: @"RandomPort"] ? -1 : [fDefaults integerForKey: @"BindPort"],
                                 TR_DEFAULT_ENCRYPTION, /* reset in prefs */
                                 TR_DEFAULT_LAZY_BITFIELD_ENABLED,
                                 NO, /* reset in prefs */

macosx/Defaults.plist
@@ -120,6 +120,8 @@
 	<false/>
 	<key>QueueSeedNumber</key>
 	<integer>3</integer>
+	<key>RandomPort</key>
+	<false/>
 	<key>RatioCheck</key>
 	<false/>
 	<key>RatioLimit</key>

macosx/PrefsController.h
@@ -66,6 +66,7 @@
 + (tr_handle *) handle;
 
 - (void) setPort: (id) sender;
+- (void) randomPort: (id) sender;
 - (void) setNat: (id) sender;
 - (void) updatePortStatus;
 - (void) portCheckerDidFinishProbing: (PortChecker *) portChecker;

commit 034696e7e1531919a3bc83d7582a502e247bc7ed

#1589 Fix a forwarding bug introduced in r7352 random port
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@7358 f4695dd4-2c0a-0410-b89c-da849a56a58e

macosx/PrefsController.m
@@ -349,7 +349,7 @@ tr_handle * fHandle;
     tr_port port = tr_sessionSetPeerPortRandom(fHandle);
     
     [fPortField setIntValue: port];
-    [self setPort: nil];
+    [self setPort: fPortField];
 }
 
 - (void) setNat: (id) sender