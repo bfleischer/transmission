bfleischer/transmission

commit 3705cc68111b6e188ea8fe3952cf1afc45cb2603

#2787 allow dragging of URLs onto the dock icon once again
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@9996 f4695dd4-2c0a-0410-b89c-da849a56a58e

macosx/Controller.m
@@ -345,14 +345,6 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
         
         tr_sessionSetRPCCallback(fLib, rpcCallback, self);
         
-        //register for dock icon drags
-        [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
-            forEventClass: kCoreEventClass andEventID: kAEOpenContents];
-        
-        //register for magnet URLs
-        [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
-        forEventClass: kInternetEventClass andEventID: kAEGetURL];
-        
         [GrowlApplicationBridge setGrowlDelegate: self];
         
         [[UKKQueue sharedFileWatcher] setDelegate: self];
@@ -577,6 +569,14 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
 - (void) applicationDidFinishLaunching: (NSNotification *) notification
 {
     [NSApp setServicesProvider: self];
+    
+    //register for dock icon drags (has to be in applicationDidFinishLaunching: to work)
+    [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
+        forEventClass: kCoreEventClass andEventID: kAEOpenContents];
+    
+    //register for magnet URLs
+    [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
+        forEventClass: kInternetEventClass andEventID: kAEGetURL];
     
     //auto importing
     [self checkAutoImportDirectory];

commit 0205557bc25fc85f137f4f4edd7af38cebde00db

fix magnet links when app is closed that I broke in r9996
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@9998 f4695dd4-2c0a-0410-b89c-da849a56a58e

macosx/Controller.m
@@ -327,6 +327,10 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
         
         [NSApp setDelegate: self];
         
+        //register for magnet URLs (has to be in init)
+        [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
+            forEventClass: kInternetEventClass andEventID: kAEGetURL];
+        
         fTorrents = [[NSMutableArray alloc] init];
         fDisplayedTorrents = [[NSMutableArray alloc] init];
         
@@ -573,10 +577,6 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
     //register for dock icon drags (has to be in applicationDidFinishLaunching: to work)
     [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
         forEventClass: kCoreEventClass andEventID: kAEOpenContents];
-    
-    //register for magnet URLs
-    [[NSAppleEventManager sharedAppleEventManager] setEventHandler: self andSelector: @selector(handleOpenContentsEvent:replyEvent:)
-        forEventClass: kInternetEventClass andEventID: kAEGetURL];
     
     //auto importing
     [self checkAutoImportDirectory];