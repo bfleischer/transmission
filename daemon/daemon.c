bfleischer/transmission

commit 38afdb31fcaae3bf23ff12148fffeb804fd33690

(trunk) #3141 "Web client should be disabled by default" -- implemented in trunk for 2.0
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@10485 f4695dd4-2c0a-0410-b89c-da849a56a58e

daemon/daemon.c
@@ -119,6 +119,7 @@ gotsig( int sig )
             const char * configDir = tr_sessionGetConfigDir( mySession );
             tr_inf( "Reloading settings from \"%s\"", configDir );
             tr_bencInitDict( &settings, 0 );
+            tr_bencDictAddBool( &settings, TR_PREFS_KEY_RPC_ENABLED, TRUE );
             tr_sessionLoadSettings( &settings, configDir, MY_NAME );
             tr_sessionSet( mySession, &settings );
             tr_bencFree( &settings );

libtransmission/session.c
@@ -273,7 +273,7 @@ tr_sessionGetDefaultSettings( const char * configDir, tr_benc * d )
     tr_bencDictAddBool( d, TR_PREFS_KEY_RENAME_PARTIAL_FILES,     TRUE );
     tr_bencDictAddBool( d, TR_PREFS_KEY_RPC_AUTH_REQUIRED,        FALSE );
     tr_bencDictAddStr ( d, TR_PREFS_KEY_RPC_BIND_ADDRESS,         "0.0.0.0" );
-    tr_bencDictAddBool( d, TR_PREFS_KEY_RPC_ENABLED,              TRUE );
+    tr_bencDictAddBool( d, TR_PREFS_KEY_RPC_ENABLED,              FALSE );
     tr_bencDictAddStr ( d, TR_PREFS_KEY_RPC_PASSWORD,             "" );
     tr_bencDictAddStr ( d, TR_PREFS_KEY_RPC_USERNAME,             "" );
     tr_bencDictAddStr ( d, TR_PREFS_KEY_RPC_WHITELIST,            TR_DEFAULT_RPC_WHITELIST );

commit 246cf9575db59d3d4dad5d6765979c7a2f3ec622

(trunk daemon) #3141 "Web client should be disabled by default" -- fix bug introduced by r10485 where some users could wind up starting a daemon that did not listen to RPC commands
git-svn-id: svn://svn.transmissionbt.com/Transmission/trunk@10542 f4695dd4-2c0a-0410-b89c-da849a56a58e

daemon/daemon.c
@@ -314,6 +314,7 @@ main( int argc, char ** argv )
 
     /* load settings from defaults + config file */
     tr_bencInitDict( &settings, 0 );
+    tr_bencDictAddBool( &settings, TR_PREFS_KEY_RPC_ENABLED, TRUE );
     configDir = getConfigDir( argc, (const char**)argv );
     loaded = tr_sessionLoadSettings( &settings, configDir, MY_NAME );
 